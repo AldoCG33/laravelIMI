e789be6a7a0ac9c960e67d0e005d2f03f551070e
jorgeA30 <al222211481@gmail.com>
2024-08-04 22:22:45 -0600

    validacion login

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -17,13 +17,12 @@ class RegisterController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
+
         ]);
 
         User::create([
-            'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
